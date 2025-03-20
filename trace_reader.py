3431ecaf7ea3196a4e2ab9fa5ce0c8456b9a4e40
Danil_Bren <danilbren1@gmail.com>
2025-03-20 16:40:37 +0100

    regex for parsing

--- trace_reader.py
+++ trace_reader.py
@@ -2,57 +2,75 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+import regex as re
 
-with open('./trace.txt') as f:
-    lines = f.readlines()
-    data = pd.DataFrame()
-    data['#'] = [line[0:5].strip() for line in lines]
-    data['thread'] = [line[5:9].strip() for line in lines]
-    data['Action type'] = [line[10:26].strip() for line in lines]
-    data['MO'] = [line[26:35].strip() for line in lines]
-    data['Location'] = [line[35:54].strip() for line in lines]
-    data['Value'] = [line[54:74].strip() for line in lines]
-    data['RF'] = [line[74:78].strip() for line in lines]
-    print(f'df: \n{data}')
+def read_from_file(path: str):
+    pattern = r'^(\d+)\s+(\d+)\s+(\w+\s\w+)\s+(\w+)\s+([\dA-F]+)\s+([\da-fx]+)\s+((\d*))\s+\(([\d,\s]+)\)$'
+    row_list = []
 
-    G = nx.DiGraph()
+    with open(path, 'r') as f:
+        lines = [line.strip() for line in f if line.strip()]
+        for line in lines:
+            match = re.match(pattern, line.strip())
+            
+            if match:
+                row = {
+                    '#': int(match.group(1)),
+                    'thread': int(match.group(2)),
+                    'Action type': match.group(3).strip(),
+                    'MO': match.group(4),
+                    'Location': match.group(5),
+                    'Value': match.group(6),
+                    'RF': int(match.group(7)) if match.group(7) else -1
+                }
+                row_list.append(row)
+            else:
+                raise RuntimeError('unexpected line format')
+    return pd.DataFrame(row_list)
+        
 
-    for instr in data['#']:
-        G.add_node(instr)
 
-    edge_labels = {}
+data = read_from_file('./trace.txt')
+print(f'df: \n{data}')
 
-    # Operation order edges
-    op_rels = data.groupby('thread')['#'].apply(list)
-    for ops in op_rels:
-        for op in range(len(ops)-1):
-            print(f'OP: {ops[op]} to {ops[op+1]}')
-            G.add_edge(ops[op], ops[op+1])
-            edge_labels[(ops[op], ops[op+1])] = 'op'
+G = nx.DiGraph()
 
-    # RF edges
-    for i in range(len(data)):
-        if data['RF'][i] != '':
-            print(f'RF: {data['RF'][i]} from {data['#'][i]}')
-            G.add_edge(data['#'][i], data['RF'][i])
-            edge_labels[data['#'][i], data['RF'][i]] = 'rf'
+for instr in data['#']:
+    G.add_node(instr)
 
-    # Thread spawning edges, not sure if correct :o
-    for i in range(len(data)):
-        if data['Action type'][i] == 'thread create':
-            for j in range(i, len(data)):
-                if data['Action type'][j] == 'thread start':
-                    f = data['#'][i]
-                    t = data['#'][j]
-                    print(f'Thread spawn: {f} to {t}')
-                    G.add_edge(f, t)
-                    edge_labels[f, t] = 'asw'
-                    break
+edge_labels = {}
 
-    # Giant mess with the types, oops
-    pos = {str(i+1): [int(data['#'][i]), int(data['thread'][i])] for i in range(len(data))}
-    print(f'pos: {pos}')
-    nx.draw_networkx(G, pos)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-    plt.show()
+# Operation order edges
+op_rels = data.groupby('thread')['#'].apply(list)
+for ops in op_rels:
+    for op in range(len(ops)-1):
+        print(f'OP: {ops[op]} to {ops[op+1]}')
+        G.add_edge(ops[op], ops[op+1])
+        edge_labels[(ops[op], ops[op+1])] = 'op'
+
+# RF edges
+for i in range(len(data)):
+    if data['RF'][i] != -1:
+        print(f'RF: {data['RF'][i]} from {data['#'][i]}')
+        G.add_edge(data['#'][i], data['RF'][i])
+        edge_labels[data['#'][i], data['RF'][i]] = 'rf'
+
+# Thread spawning edges, not sure if correct :o
+for i in range(len(data)):
+    if data['Action type'][i] == 'thread create':
+        for j in range(i, len(data)):
+            if data['Action type'][j] == 'thread start':
+                f = data['#'][i]
+                t = data['#'][j]
+                print(f'Thread spawn: {f} to {t}')
+                G.add_edge(f, t)
+                edge_labels[f, t] = 'asw'
+                break
+
+# Giant mess with the types, oops
+pos = {str(i+1): [int(data['#'][i]), int(data['thread'][i])] for i in range(len(data))}
+print(f'pos: {pos}')
+nx.draw_networkx(G, pos)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+plt.show()
 
