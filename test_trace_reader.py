00738708aea8497a0c6913597fddd498676e26bb
Toon Kling <t.kling@student.tudelft.nl>
2025-04-02 12:34:01 +0200

    test: Add second trace case

--- test_trace_reader.py
+++ test_trace_reader.py
@@ -3,13 +3,12 @@ from trace_reader import find_data_race
 
 class TestFindDataRace(unittest.TestCase):
     def test_data_race_case_1(self):
-        dir = 'races_traces/simple1.txt'
-        result = find_data_race(dir)
+        result = find_data_race('races_traces/simple1.txt')
         self.assertEqual(result, (0, 6))
 
-    # def test_data_race_case_2(self):
-    #     result = find_data_race(input_data_2)
-    #     self.assertEqual(result, expected_output_2)
+    def test_data_race_case_2(self):
+        result = find_data_race('races_traces/simple2.txt')
+        self.assertEqual(result, None)
 
 if __name__ == '__main__':
     unittest.main()
