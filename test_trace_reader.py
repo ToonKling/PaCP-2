79f2576871d2d1c5b1813090bbae95cc00c20daf
Aleksandra Jach <a.m.jach@student.tudelft.nl>
2025-04-12 15:17:48 +0200

    Add unit tests for custom programs

--- test_trace_reader.py
+++ test_trace_reader.py
@@ -2,21 +2,62 @@ import unittest
 from trace_reader import find_data_race
 
 class TestFindDataRace(unittest.TestCase):
+
+    # Program 1 from overleaf
     def test_data_race_case_1(self):
         result = find_data_race('races_traces/simple1.txt')
         self.assertIn((6, 8), result)
 
+    # Program 1 from overleaf
     def test_data_race_case_2(self):
         result = find_data_race('races_traces/simple2.txt')
         self.assertEqual(result, [])
 
+    # Program 2 from overleaf
+    def test_rel_acq_no_race(self):
+        result = find_data_race('./races_traces/rel_acq_no_race1.txt')
+        self.assertEqual(result, [])
+
+    # Program 3 from overleaf
+    def test_program3(self):
+        result1 = find_data_race('./races_traces/loops1.txt')
+        result2 = find_data_race('./races_traces/loops2.txt')
+        self.assertIn((9, 22), result1)
+        self.assertIn((10, 7), result2)
+
+    # Program 4 from overleaf
+    def test_program4(self):
+        result1 = find_data_race('./races_traces/mp.txt')
+        self.assertIn((9, 11), result1)
+
+    # Program 5 from overleaf
+    def test_program5(self):
+        result1 = find_data_race('./races_traces/reorder1.txt')
+        result2 = find_data_race('./races_traces/reorder2.txt')
+        self.assertIn((9, 22), result1)
+        self.assertIn((9, 12), result2)
+
+    # Program 6 from overleaf
+    def test_program6(self):
+        result1 = find_data_race('./races_traces/seq_cst_no_race1.txt')
+        self.assertEqual(result1, [])
+
+    # Program 7 from overleaf
+    def test_program7(self):
+        result1 = find_data_race('./races_traces/reorder_seq_cst1.txt')
+        result2 = find_data_race('./races_traces/reorder_seq_cst2.txt')
+        self.assertIn((23, 25), result1)
+        self.assertEqual(result2, [])
+
+    # Program 8 is a To do from overleaf
+   
     def test_double_write_1(self):
         result = find_data_race('./races_traces/double_write_race1.txt')
         self.assertIn((6, 8), result)
 
     def test_double_write_2(self):
         result = find_data_race('./races_traces/double_write_race2.txt')
-        self.assertIn((6, 9), result)
+        self.assertIn((9, 10), result)
 
     def test_double_write_no_race1(self):
         result = find_data_race('./races_traces/double_write_no_race1.txt')
@@ -26,10 +67,6 @@ class TestFindDataRace(unittest.TestCase):
         result = find_data_race('./races_traces/double_write_no_race2.txt')
         self.assertEqual(result, [])
 
-    # Program 2 from overleaf
-    def test_rel_acq_no_race(self):
-        result = find_data_race('./races_traces/rel_acq_no_race1.txt')
-        self.assertEqual(result, [])
 
     def test_iriw_1(self):
         result = find_data_race('./races_traces/iriw1.txt')
--- trace_reader.py
+++ trace_reader.py
@@ -112,9 +112,9 @@ def create_graph(data, rf_edges, hb_edges, swa_relation, to = None, fr = None, d
 
 
 def find_data_race(fileName: str, draw_graph: bool = False) -> list[tuple[int, int]]:
-    print(f'Finding the data race')
+    # print(f'Finding the data race')
     raw_lines = read_from_file(fileName)
-    print(f'Read the file')
+    # print(f'Read the file')
 
     node_to_thread = dict()
     last_seen_thread = dict()  # thread ID to last seen instruction ID
@@ -243,7 +243,7 @@ def find_data_race(fileName: str, draw_graph: bool = False) -> list[tuple[int, i
                 exclude_self = writes_same_loc[writes_same_loc['#'] != node_id]
                 for potential_race_id in exclude_self['#']:
                         if not path_exists(hb_relations, potential_race_id, node_id):
-                            print(f'DATA RACE: {potential_race_id} and {node_id} both access {mem_loc} without a HB relation')
+                            # print(f'DATA RACE: {potential_race_id} and {node_id} both access {mem_loc} without a HB relation')
                             # create_graph(data, rf_edges=rf_relations, hb_edges=hb_relations, swa_relation=sw_relations, draw_graph=True)
                             data_races.append((potential_race_id, node_id))
             case 'atomic read':
@@ -252,7 +252,7 @@ def find_data_race(fileName: str, draw_graph: bool = False) -> list[tuple[int, i
                 if not (node_id not in not_ordered_memory_locations and \
                         node_from not in not_ordered_memory_locations) and \
                 node_from in node_write and not path_exists(hb_relations, node_from, node_id):
-                    print(f"DATA RACE between nodes {node_from} and {node_id}")
+                    # print(f"DATA RACE between nodes {node_from} and {node_id}")
                     # create_graph(data, rf_edges=rf_relations, hb_edges=hb_relations, swa_relation=sw_relations, draw_graph=False)
                     data_races.append((node_from, node_id))
             case _: pass
@@ -262,4 +262,4 @@ def find_data_race(fileName: str, draw_graph: bool = False) -> list[tuple[int, i
 
 if __name__ == "__main__":
     races = find_data_race('./output.txt', draw_graph=True)
-    print(f'Found races: {races}')
+    # print(f'Found races: {races}')
